850dc90390aeb646439ce02044e7c72c9a03b345
Kim Chanho <109461257+wapago@users.noreply.github.com>
2023-04-24 10:14:44 +0900

    Update Game_refactoring01.java
    랜덤숫자생성로직 수정

--- Game_refactoring01.java
+++ Game_refactoring01.java
@@ -20,21 +20,20 @@ public class Game_refactoring01 {
 		generateRndNumber();
 	}
 	
-	public static void generateRndNumber() {
+	public static Integer[] generateRndNumber() {
 		List<Integer> oneToNine = new ArrayList<>();
 		Integer[] rndNumberArr = new Integer[3];
-		String[] rndNumber = new String[3];
 		
 		for(int i=1; i<=9; i++) {
 			oneToNine.add(i);
 		}
+		
 		Collections.shuffle(oneToNine);
 		
 		for(int i=0; i<3; i++) {
 			rndNumberArr[i] = oneToNine.get(i);
-			rndNumber[i] = String.valueOf(rndNumberArr[i]);
 		}
 		
-		answer = String.join("", rndNumber);
+		return rndNumberArr;
 	}
 }
